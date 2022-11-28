f41d4c5ff67dc93952647eed6596e8ffd11a08be
이종우 <@192.168.219.120>
2022-11-28 15:00:07 +0900

    12345sad

---
+++ lupy/WebContent/js/day01/test06.js
@@ -0,0 +1,18 @@
+var num;
+var obj = null;
+
+document.getElementById('txt').innerHTML =  '100 type : ' + typeof 100 + '<br>';
+document.getElementById('txt').innerHTML =
+	dcument.getElementById('txt').innerHTML + '3.14 type : ' + typeof 3.14 + '<br>';
+document.getElementById('txt').innerHTML =
+	dcument.getElementById('txt').innerHTML + '"dooly" type : ' + typeof 'dooly' + '<br>';
+document.getElementById('txt').innerHTML =
+	dcument.getElementById('txt').innerHTML + '"true"" type : ' + typeof true + '<br>';
+document.getElementById('txt').innerHTML =
+	dcument.getElementById('txt').innerHTML + '100 type : ' + typeof 100 + '<br>';  '3.14 type : ' + typeof 3.14 + '<br>';
+document.getElementById('txt').innerHTML = 
+	dcument.getElementById('txt').innerHTM + '{name: "루피", job: "해적"}type : ' + typeof {name: "루피", job: "해적"} + '<br>';
+document.getElementById('txt').innerHTML = 
+	document.getElementById('txt').innerHTML = + 'num type : ' + typeof num + '<br>';
+document.getElementById('txt').innerHTML = xxxxxxxxxxxx
+	document.getElementById('txt').innerHTML = + 'num type : ' + typeof num + '<br>';
